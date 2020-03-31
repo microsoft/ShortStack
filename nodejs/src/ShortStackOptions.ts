b97b3d2a0935375412d5092b178c51051d6f4de8
nebosite <ericjorg@thejcrew.net>
2020-03-31 10:27:17 -0700

    Added initial help

--- nodejs/src/ShortStackOptions.ts
+++ nodejs/src/ShortStackOptions.ts
@@ -3,8 +3,8 @@
 //------------------------------------------------------------------------------
 export class ShortStackOptions
 {
-    shouldMoo = true;
-    brainSize: string | undefined = undefined;
+    showHelp = false;
+    helpOption: string | undefined = undefined;
     badArgs = new Array<string>();
 
     //------------------------------------------------------------------------------
@@ -12,17 +12,44 @@ export class ShortStackOptions
     //------------------------------------------------------------------------------
     constructor(argv: string[])
     {
-        for(let i =2; i < argv.length; i++)
+        if(argv.length == 0)
         {
-            // commandline is -name=value  (or /name=value), value is optional
-            let trimmed = argv[i].replace(/^([-/]*)/, "").toLowerCase();
-            let parts = trimmed.split('=');
-            switch(parts[0])
-            {
-                case "brainsize": this.brainSize = parts[1]; break;
-                case "moo": this.shouldMoo = true; break;
-                default: this.badArgs.push(argv[i]); break;
-            }
+            this.showHelp = true;
+            return;
+        }
+
+        // commandline is -name=value  (or /name=value), value is optional
+        const argument = this.getArgParts(argv[0]);
+        switch(argument.name)
+        {
+            case "h": 
+            case "help": 
+            case "?": this.processHelp(argv.slice(1)); break;
+            default: this.badArgs.push(argv[0]); break;
+        }
+        
+    }
+
+    //------------------------------------------------------------------------------
+    // break -FOO=Bar into {name:"foo", value: "Bar"}
+    //------------------------------------------------------------------------------
+    getArgParts(argument: string)
+    {
+        const trimmed = argument.replace(/^([-/]*)/, "");
+        const parts = trimmed.split('=',2);
+        if(parts.length == 2) parts[0] = parts[0].toLowerCase();
+        return {name: parts[0], value: parts.length == 2 ? parts[1] : undefined}
+    }
+
+    //------------------------------------------------------------------------------
+    // ctor
+    //------------------------------------------------------------------------------
+    processHelp(argv: string[])
+    {
+        this.showHelp = true;
+        if(argv.length > 0)
+        {
+            this.helpOption = this.getArgParts(argv[0]).name;
         }
     }
 }
--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -4,20 +4,29 @@ import chalk from "chalk"
 //------------------------------------------------------------------------------
 // main
 //------------------------------------------------------------------------------
-async function main(options: ShortStackOptions) {
-    if(options.badArgs.length > 0)
-    {
-        console.log("ERROR: Bad arguments: ");
-        options.badArgs.forEach(arg => console.log(`  ${arg}`));
-        process.exit();
-    }
-    
+async function main(argv: string[]) {
+
     process.on("SIGINT", async () => {
         console.log("*** Process was interrupted! ***")
         process.exit(1);
     });
 
     try {
+        const options = new ShortStackOptions(argv);
+       
+        if(options.showHelp)
+        {
+            showHelp(options);
+            return 0;
+        }
+
+        if(options.badArgs.length > 0)
+        {
+            console.log("ERROR: Bad arguments: ");
+            options.badArgs.forEach(arg => console.log(`  ${arg}`));
+            process.exit();
+        }   
+
         // TODO: Run your application code here with your options
         // await myObject.doSomething();
         return 0;
@@ -28,12 +37,28 @@ async function main(options: ShortStackOptions) {
 }
 
 //------------------------------------------------------------------------------
+// show help
+//------------------------------------------------------------------------------
+function showHelp(options: ShortStackOptions)
+{
+    if(!options.helpOption)   
+    {
+        console.log("ShortStack is a tool for handling a stacked pull request workflow.")
+        console.log("")
+        console.log("for more information:")
+        console.log(chalk.whiteBright("    ss help commands      ") + "Show available commands");
+        console.log(chalk.whiteBright("    ss help workflow      ") + "Describe the ss workflow");
+        console.log(chalk.whiteBright("    ss help setup         ") + "Instructions on how to set up your environment for stacked PRs");
+    }
+}
+
+//------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 
 console.log(chalk.whiteBright('SHORTSTACK v0.01.00'))
 
-main(new ShortStackOptions(process.argv))
+main(process.argv.slice(2))
     .then(status => {
         //console.log(`Exiting with status: ${status}`)
         process.exit(status);
